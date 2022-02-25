78ce029a06068d73549db84cd85074c5c3c56e96
Fanta335 <fantayusuke5210@gmail.com>
2022-02-25 22:40:30 +0900

    '#3 Add User module'

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDTO: CreateUserDTO) {
+    return this.usersService.create(createUserDTO);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreateUserDTO } from './dto/user.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(newUser: CreateUserDTO): Promise<User> {
+    const user = this.usersRepository.create(newUser);
+    const result = await this.usersRepository.save(user);
+    return result;
+  }
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.usersRepository.findOne(id);
+  }
+
+  async remove(id: string): Promise<DeleteResult> {
+    const result = await this.usersRepository.delete(id);
+    return result;
+  }
+}
