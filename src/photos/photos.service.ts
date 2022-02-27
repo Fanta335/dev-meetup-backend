a0f471ac982d0275475ea9d2e741d4bd7d344232
Fanta335 <fantayusuke5210@gmail.com>
2022-02-27 22:24:14 +0900

    #5 Add Photo module

--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreatePhotoDTO } from './dto/photo.dto';
+import { Photo } from './photo.entity';
 
 @Injectable()
-export class PhotosService {}
+export class PhotosService {
+  constructor(
+    @InjectRepository(Photo)
+    private photosRepository: Repository<Photo>,
+  ) {}
+
+  async create(newPhoto: CreatePhotoDTO) {
+    const photo = this.photosRepository.create(newPhoto);
+    const result = await this.photosRepository.save(photo);
+    return result;
+  }
+
+  findAll(): Promise<Photo[]> {
+    return this.photosRepository.find();
+  }
+
+  findOne(id: string): Promise<Photo> {
+    return this.photosRepository.findOne(id);
+  }
+
+  async remove(id: string): Promise<DeleteResult> {
+    const result = await this.photosRepository.delete(id);
+    return result;
+  }
+}
