a88a622d16e19f14649a5638d0008b895c38736e
Fanta335 <fantayusuke5210@gmail.com>
2022-03-24 23:58:07 +0900

    #27 Remove testUser

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Message } from 'src/messages/entity/message.entity';
 import { Room } from 'src/rooms/entity/room.entity';
 import { User } from 'src/users/entity/user.entity';
-import { testUser } from 'src/users/testUser';
 import { CreateMessageDTO } from './dto/createMessage.dto';
 import { MessagesService } from './messages.service';
 
@@ -13,7 +12,7 @@ export class MessagesController {
   @Post()
   create(
     @Body() createMessageDTO: CreateMessageDTO,
-    user: User = testUser,
+    user: User,
     room: Room,
   ): Promise<Message> {
     return this.messagesService.createMessage(createMessageDTO, user, room);
