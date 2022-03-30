635c83973e9974ac2e322a353304ac0894ac04ba
Fanta335 <fantayusuke5210@gmail.com>
2022-03-30 16:14:42 +0900

    Revert getuser decorator

--- src/users/get-user.decorator.ts
+++ src/users/get-user.decorator.ts
@@ -1,10 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetUserSubId = createParamDecorator(
+export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    // remove 'auth0|' prefix
-    const prefix = 'auth0|';
-    return request.user.sub.slice(prefix.length);
+    console.log(request.user);
+    return request.user;
   },
 );
