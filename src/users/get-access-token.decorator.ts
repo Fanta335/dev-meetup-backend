4c365ad24b47c9f98d0673223e8094503307da9a
Fanta335 <fantayusuke5210@gmail.com>
2022-04-04 18:12:01 +0900

    Rename decorator: getUser -> getAccessToken

--- src/users/get-access-token.decorator.ts
+++ src/users/get-access-token.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetUser = createParamDecorator(
+export const GetAccessToken = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     console.log(request.user);
