30edaadaa804d94d5879585b068fb44cc6b28db3
Fanta335 <fantayusuke5210@gmail.com>
2022-04-05 16:10:59 +0900

    Improve type difinition: UserAccessToken

--- src/users/types/index.ts
+++ src/users/types/index.ts
@@ -1,4 +1,20 @@
+const namespace = process.env.AUTH0_NAMESPACE;
+const roles = Symbol(namespace + '/roles');
+const mysqlUser = Symbol(namespace + '/mysqlUser');
+
+export type MysqlUser = {
+  id: number;
+  subId: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+};
+
 export type UserAccessToken = {
+  [roles]: string[];
+  [mysqlUser]: MysqlUser;
   iss: string;
   sub: string;
   aud: string[];
