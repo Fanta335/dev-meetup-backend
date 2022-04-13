e0fcbe8dd6dcc2beda615d188c0b5462e002439d
Fanta335 <fantayusuke5210@gmail.com>
2022-04-13 20:29:54 +0900

    Add decorators: getWsData, GetJwtFromWsClient

--- src/users/get-access-token.decorator.ts
+++ src/users/get-access-token.decorator.ts
@@ -6,3 +6,19 @@ export const GetAccessToken = createParamDecorator(
     return request.user;
   },
 );
+
+export const GetWsData = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const client = ctx.switchToWs().getClient();
+    const resultData = ctx.switchToWs().getData();
+    console.log('client: ', client, 'resultData: ', resultData);
+  },
+);
+
+export const GetJwtFromWsClient = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const jwt: string = ctx.switchToWs().getClient().handshake.auth.token;
+    // console.log('jwt: ', jwt);
+    return jwt;
+  },
+);
