5ad14e8333c3d45b300f263de404615135c98b33
Fanta335 <fantayusuke5210@gmail.com>
2022-04-16 13:30:10 +0900

    Add isOptional decorator

--- src/users/dto/updateUser.dto.ts
+++ src/users/dto/updateUser.dto.ts
@@ -1,6 +1,6 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDTO {
-  @IsString() name: string;
-  @IsEmail() email: string;
+  @IsString() @IsOptional() name?: string;
+  @IsEmail() @IsOptional() email?: string;
 }
