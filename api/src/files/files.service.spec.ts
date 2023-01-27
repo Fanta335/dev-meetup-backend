4bf6018eafdfd677716775077b5fba3c5c4414f3
Fanta335 <fantayusuke5210@gmail.com>
2023-01-27 22:25:07 +0900

    Fix `not-found` errors with temporary codes (#34)

--- api/src/files/files.service.spec.ts
+++ api/src/files/files.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesService } from './files.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PublicFile } from './entity/publicFile.entity';
+import { ConfigService } from '@nestjs/config';
 
 describe('FilesService', () => {
   let service: FilesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilesService],
+      providers: [
+        FilesService,
+        {
+          provide: getRepositoryToken(PublicFile),
+          useValue: {},
+        },
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<FilesService>(FilesService);
--- api/src/invitations/invitations.service.spec.ts
+++ api/src/invitations/invitations.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvitationsService } from './invitations.service';
+import { InvitationRepository } from './entity/invitation.repository';
+import { RoomsRepository } from 'src/rooms/entity/room.repository';
 
 describe('InvitationsService', () => {
   let service: InvitationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InvitationsService],
+      providers: [
+        InvitationsService,
+        {
+          provide: InvitationRepository,
+          useValue: {},
+        },
+        {
+          provide: RoomsRepository,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<InvitationsService>(InvitationsService);
